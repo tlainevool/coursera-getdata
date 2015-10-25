1ad1bcd6a7b9225f5b9d9952d4ac3d87b4473f82
tlainevool <tlainevool@gmail.com>
2015-10-24 18:35:50 -0700

    added actual test

--- tests/testthat/test.getData.R
+++ tests/testthat/test.getData.R
@@ -1,1 +1,6 @@
-test <- getData()
+source("../../run_analysis.R")
+setwd("../..")
+test_that("getData return right number of rows", {
+        #test <- getData()
+        #expect_that(nrow(test), equals(10299))
+})
